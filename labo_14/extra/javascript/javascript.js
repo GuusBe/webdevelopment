b583e2c5fe0bf2115560e1a147e549113ba8efa8
GuusBe <guus.behaeghe@student.vives.be>
2026-02-10 16:36:26 +0100

    qsdfghjklm

--- labo_14/extra/javascript/javascript.js
+++ labo_14/extra/javascript/javascript.js
@@ -1,7 +1,10 @@
 const vergelijk = () =>{
     let getal1 = Number(document.getElementById("getal1").value);
     let getal2 = Number(document.getElementById("getal2").value);
-    let vergelijk = (getal1 === getal2) ? console.log((getal1 + getal2) * 3) : console.log(getal1 + getal2);
+    let txtOutput = document.getElementById("Input");
+    let vergelijk = (getal1 === getal2) ? txtOutput.innerText = ((getal1 + getal2) * 3) : txtOutput.innerText = (getal1 + getal2);
+
+
 }
 let button = document.getElementById("button");
 button.addEventListener("click", vergelijk)
